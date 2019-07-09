32e0a1c883cecf7a70cf1cfacf9a5e821dd0db7e
github username <github email address>
2019-07-08 22:27:44 -0600

    update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+        redirect_to current_user if current_user
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save 
+            session[:user_id] = @user.id 
+            redirect_to @user 
+        else
+            redirect_to root_path
+        end
+    end
+
+    def show 
+        @user = User.find(params[:id])
+    end
+    
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
