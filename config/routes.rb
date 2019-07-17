0a3c556a55f5f95c88e2ab10528ce46f3d9d2610
Kelsey Low <kklowlow@gmail.com>
2019-07-17 22:59:13 +0000

    update sessions home view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  #root 'sessions#home'
   get '/', to: 'sessions#home'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :comments
   resources :rootes
-  resources :users
+  resources :users, except: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
