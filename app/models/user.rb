d2d038aa144ab3790b498acdf42903954109e5da
Kelsey Low <kklowlow@gmail.com>
2019-07-17 22:06:43 +0000

    update associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
 
     has_many :authored_rootes, foreign_key: 'author_id', class_name: 'Roote'
     has_many :comments
-    has_many :commented_rootes, through: :comments, source: 'roote'
+    has_many :commented_rootes, through: :comments, source: 'Roote'
 end
