d0fd99e849981d2577dc6f5476f71cfd92dad263
github username <github email address>
2019-07-18 11:18:12 -0600

    implement oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,8 +17,24 @@ class SessionsController < ApplicationController
     end
   end
 
+  def oauth_create
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = oauth['info']['name']
+      u.email = oauth['info']['email']
+      u.password = SecureRandom.hex
+    end 
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
   def logout
     session.clear
     redirect_to '/'
+  end
+
+  private 
+
+  def oauth 
+    request.env['omniauth.auth']
   end
 end
