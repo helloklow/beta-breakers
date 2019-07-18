7ad5fc3ed0529421caf593319f4a37f6c9bfb45a
github username <github email address>
2019-07-17 18:51:32 -0600

    update users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
 
   get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
 
   resources :comments
   resources :rootes
-  resources :users, except: [:new, :create]
+  resources :users, except: :new
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
