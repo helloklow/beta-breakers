983f4d2707e2e7d51e305027ac98981d7548c054
github username <github email address>
2019-07-17 20:04:27 -0600

    update signup route and form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-
   def home
   end
 
@@ -11,5 +10,4 @@ class SessionsController < ApplicationController
 
   def logout
   end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
 
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :comments
   resources :rootes
-  resources :users, except: :new
+  resources :users, except: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
