21add88d6bf1b4747b3bfb9606252eee7c63c5fe
github username <github email address>
2019-07-17 19:04:07 -0600

    add users show template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id 
             redirect_to @user 
         else
-            redirect_to root_path
+            render :new
         end
     end
 
