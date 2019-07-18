267b3ac16e02acc9c02d5e8ce917e6664a5124af
github username <github email address>
2019-07-17 18:36:34 -0600

    update users controller ax and users signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
     end
 
     def create 
-        #@user = User.new(user_params)
-        #if @user.save 
-            #session[:user_id] = @user.id 
-            #redirect_to @user 
-        #else
-            #redirect_to root_path
-        #end
+        @user = User.new(user_params)
+        if @user.save 
+            session[:user_id] = @user.id 
+            redirect_to @user 
+        else
+            redirect_to root_path
+        end
     end
 
     def show 
