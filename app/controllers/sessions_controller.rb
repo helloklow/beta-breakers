d8d7757d25af5668c5d423fdeecf3de548992c27
github username <github email address>
2019-07-17 20:29:27 -0600

    update session controller and login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,24 @@
 class SessionsController < ApplicationController
   def home
+    render :home
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(username: params[:user][:username]) # form_for creates nested hash!
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id 
+      redirect_to user_path(@user)
+    else 
+      render :new
+    end
   end
 
   def logout
+    session.clear
+    redirect_to '/'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
     def new
         @user = User.new
-        #redirect_to current_user if current_user
     end
 
     def create 
