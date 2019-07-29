8998397576b9df9280f7a70f2423f3ccece313da
github username <github email address>
2019-07-29 14:49:41 -0600

    re-implement search feature

--- app/controllers/rootes_controller.rb
+++ app/controllers/rootes_controller.rb
@@ -77,6 +77,6 @@ class RootesController < ApplicationController
     private 
 
     def roote_params 
-        params.require(:roote).permit(:name, :category, :location, :difficulty, :content, :search)
+        params.require(:roote).permit(:name, :category, :location, :difficulty, :content)
     end
 end
