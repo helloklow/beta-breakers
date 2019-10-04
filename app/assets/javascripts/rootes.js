ab559d2ffca686679f6b00a6107df8b19fc58784
github username <github email address>
2019-10-04 16:47:37 -0600

    update rootes js

--- app/assets/javascripts/rootes.js
+++ app/assets/javascripts/rootes.js
@@ -7,6 +7,32 @@ class Roote {
         this.difficulty = roote.difficulty
         this.conent = roote.content
         this.author_id = roote.author_id
+        this.comments = roote.comments
     }
+}
+
+renderRootes() [
+    return (`<a href="/rootes/${this.id}" data-id="${this.id}" class="show_link"><h1>${this.name}</br></h1></a>`)
+]
+
+renderRoote() {
+    let rooteComments = this.comments.map(comment => {
+        return (`<p>${comment.content}</p>`)
+    }).join('')
+
+    return (`<h2>${this.name}</br>Category: ${this.category}</br>Location: ${this.location}</br>Difficulty: ${this.difficulty}</br>Comments: ${rooteComments}<br></br></h2></a>`)
+}
+
+function getRootes() {
+    let rootesDiv = document.getElementById('user-rootes');
+    fetch('https://localhost:3000/rootes.json')
+    .then(resp => resp.json())
+    .then(rootes => {
+        rootesDiv.innerHTML = '';
+        for (let i = 0, len = rootes.length; i < len; ++i) {
+            const rooteObj = new Roote(rootes[i]);
+            rootesDiv.innerHTML += rooteObj.displayRoote('user_rootes');                  
+        }
+    })
 }
 
