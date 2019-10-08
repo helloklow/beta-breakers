b94fa900aa5d51fc4aafdbee3c4f0acb90ad7ec2
github username <github email address>
2019-10-08 16:29:29 -0600

    add current_user to app controller, update view user rootes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
     def set_user 
         @user = session[:user_id]
     end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
 end
